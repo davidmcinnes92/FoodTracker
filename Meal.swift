b776e0313da1a1cdfbc9e98e964020a1e21cbf5e
David McInnes <davidmcinnes92@gmail.com>
2020-06-24 08:35:18 +0100

    Update Meal.swift
    Fixed the meal logic to not allow ratings greater than 5.

--- Meal.swift
+++ Meal.swift
@@ -15,7 +15,18 @@ class Meal {
     var rating: Int
     
     // MARK: Initialization
-    init(name: String, photo: UIImage?, rating: Int) {
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
         self.name = name
         self.photo = photo
         self.rating = rating
